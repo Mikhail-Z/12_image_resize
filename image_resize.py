96c7a62bf0010c2d825d5541ee83256c1f973c14
Mikhail <mike.rzn@yandex.ru>
2017-01-19 15:38:57 +0300

    Использование конкретных аргументов в функции resize_image и использование format() для сложения строк

--- image_resize.py
+++ image_resize.py
@@ -14,39 +14,26 @@ def arg_parse():
     return args
 
 
-def error_printer(error_id):
-    if error_id == 1:
-        print("No such file or directory")
-    elif error_id == 2:
-        print("You should enter both width and height or only scale.")
-
-
-def warning_printer(warning_id):
-    if warning_id == 1:
-        print("Note, the proportion of width and height will be changed!")
-
-
 def args_check(args):
     if (args.width and args.height) and not args.scale:
         return True
     elif args.scale and not (args.width and args.height):
         return True
     else:
-        error_printer(2)
         return False
 
 
-def resize_image(args):
-    path_to_original = args.input
+def resize_image(input, output, width, height, scale):
+    path_to_original = input
     im = Image.open(path_to_original)
     if args.scale:
-        new_width, new_height = get_new_width_and_height(im.size[0], im.size[1], args.scale)
+        new_width, new_height = get_new_width_and_height(im.size[0], im.size[1], scale)
     else:
-        new_width, new_height = args.width, args.height
+        new_width, new_height = width, height
     if im.size[0]/im.size[1] != new_width/new_height:
-        warning_printer(1)
+        print("Note, the proportion of width and height will be changed!")
 
-    if not args.output:
+    if not output:
         path_to_result = get_path_to_result(path_to_original, new_width, new_height)
     else:
         path_to_result = args.output
@@ -58,7 +45,6 @@ def resize_image(args):
 def check_file_existence(path_to_original, path_to_result):
     if not(os.path.exists(path_to_original)) or \
             (path_to_result and not os.path.exists(path_to_result)):
-        error_printer(1)
         return False
 
     return True
@@ -72,13 +58,18 @@ def get_new_width_and_height(old_width, old_height, scale):
 
 def get_path_to_result(path_to_original, new_width, new_height):
     file_name, extension = os.path.splitext(path_to_original)
-    path_to_result = file_name + "_" + str(new_width) + "x" + str(new_height) + extension
-
+    path_to_result = "{filename}{0}{width}{1}{height}{ext}".format("_", "x", filename=file_name, width=new_width,
+                                                                   height=new_height, ext=extension)
     return path_to_result
 
 
 if __name__ == '__main__':
     args = arg_parse()
-    if args_check(args) and check_file_existence(args.input, args.output):
-        resize_image(args)
-        print("Done!")
+    if args_check(args):
+        if check_file_existence(args.input, args.output):
+            resize_image(args.input, args.output, args.width, args.height, args.scale)
+            print("Done!")
+        else:
+            print("No such file or directory")
+    else:
+        print("You should enter both width and height or scale!")
